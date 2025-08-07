9445b61ee66028c478fe0018aeab9e4ce223ffc8
Saul C Batista <saulcbatista@gmail.com>
2025-08-07 18:21:01 -0300

    feat: install app `companies`

--- setup/settings.py
+++ setup/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'locations.apps.LocationsConfig'
+    'locations.apps.LocationsConfig',
+    'companies.apps.CompaniesConfig'
 ]
 
 MIDDLEWARE = [
