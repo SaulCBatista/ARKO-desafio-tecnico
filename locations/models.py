08f31f969cb690993b7aa017b5f46954bc4ef408
Saul C Batista <saulcbatista@gmail.com>
2025-08-07 00:19:12 -0300

    feat: create model `cities`

--- locations/models.py
+++ locations/models.py
@@ -5,3 +5,9 @@ class State(models.Model):
     name = models.CharField(max_length=32, null=False, blank=False)
     acronym = models.CharField(max_length=2, null=False, blank=False)
     region = models.CharField(max_length=32, null=False, blank=False)
+
+class City(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32, null=False, blank=False)
+    state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='cities')
+
