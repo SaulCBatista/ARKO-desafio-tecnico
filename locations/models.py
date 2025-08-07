3e86789aeb38ed420b010cfb88723517f319bff5
Saul C Batista <saulcbatista@gmail.com>
2025-08-07 13:37:48 -0300

    feat: add `districts` model

--- locations/models.py
+++ locations/models.py
@@ -11,3 +11,9 @@ class City(models.Model):
     name = models.CharField(max_length=32, null=False, blank=False)
     state = models.ForeignKey(State, on_delete=models.CASCADE, related_name='cities')
 
+class District(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32, null=False, blank=False)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='districts')
+
+
