1e8a13ec0166d4959bed71f3d0804e784be00531
Saul C Batista <saulcbatista@gmail.com>
2025-08-07 18:19:51 -0300

    feat: create model `company`

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    basic_cnpj = models.CharField(max_length=8, unique=True)
+    corporate_name = models.CharField(max_length=200)
+    legal_nature = models.CharField(max_length=3)
+    responsible_qualification = models.CharField(max_length=2)
+    share_capital = models.DecimalField(max_digits=20, decimal_places=2)
+    company_size = models.CharField(max_length=1)             
+    federative_entity = models.CharField(max_length=1)        
+
