677e51f2555d5aa247c54f023d3ae05248712d13
Saul C Batista <saulcbatista@gmail.com>
2025-08-08 11:20:16 -0300

    feat: add `StateListView` page

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,12 @@
+from django.views.generic import ListView
 from django.shortcuts import render
+from .models import State
 
-# Create your views here.
+def dashboard_view(request):
+    return render(request, 'pages/dashboard.html')
+
+class StateListView(ListView):
+    model = State
+    template_name = 'pages/state_table.html'
+    context_object_name = 'states'
+    paginate_by = 10
