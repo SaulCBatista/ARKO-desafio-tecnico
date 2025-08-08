c568adb8247e058b247ff5085e0ccce11ea03033
Saul C Batista <saulcbatista@gmail.com>
2025-08-08 11:49:52 -0300

    feat: add `CityListView` page

--- locations/views.py
+++ locations/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView
 from django.shortcuts import render
-from .models import State
+from .models import State, City
 
 def dashboard_view(request):
     return render(request, 'pages/dashboard.html')
@@ -9,4 +9,10 @@ class StateListView(ListView):
     model = State
     template_name = 'pages/state_table.html'
     context_object_name = 'states'
+    paginate_by = 10
+
+class CityListView(ListView):
+    model = City
+    template_name = 'pages/city_table.html'
+    context_object_name = 'cities'
     paginate_by = 10
--- setup/urls.py
+++ setup/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from locations.views import StateListView, dashboard_view
+from locations.views import StateListView, CityListView, dashboard_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard_view, name='dashboard'),
-    path('states-table/', StateListView.as_view(), name='state_list')
+    path('states-table/', StateListView.as_view(), name='state_list'),
+    path('cities-table/', CityListView.as_view(), name='city_list'),
 ]
